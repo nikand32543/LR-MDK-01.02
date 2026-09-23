03013219b9ea2c633024e0e0bf50237c0480be62
vlad <smirnovvlad751@gmail.com>
2026-09-23 22:50:24 +0300

    Сделали тесты на удачное и неудачное добавление пользователя

--- moq/UnitTest/UnitTest1.cs
+++ moq/UnitTest/UnitTest1.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System;
+using System.Collections.Generic;
 using UnitTestLib;
 
 namespace UnitTest
@@ -8,6 +9,44 @@ namespace UnitTest
     [TestClass]
     public class UnitTest1
     {
+        [TestMethod]
+        public void TestMethod2() // если файл вернул одного валидного пользователя  и в БД его нет то ImportUser вернёт true
+        {
+            List<User> users = new List<User>
+            {
+                new User { Login = "login123", Password = "123", Name = "Ivan", LastName = "Ivanov" }
+            };
+
+            var fileMock = new Mock<IUserFile>(); // мок обьект для интерфейса
+            fileMock.Setup(files => files.ReadAllLines("users.txt")).Returns(users);
+            var repoMock = new Mock<IUsersRepositoriy>();
+            repoMock.Setup(repo => repo.GetUser("login123")).Returns((User)null);
+            IUserFile file = fileMock.Object; // получение заглушки moq
+            IUsersRepositoriy repository = repoMock.Object;
+            ImportFromFile importer = new ImportFromFile(file, repository);
+            bool Flag = importer.ImportUser("users.txt");
+            Assert.IsTrue(Flag);
+
+        }
+        [TestMethod]
+        public void TestMethod_UnsuccessfulImport() // неудачный импорт пользователь с пробелом  пропущен, в БД ничего
+        {
+            List<User> users = new List<User>
+            {
+                new User { Login = "i van", Password = "123", Name = "Ivan", LastName = "Ivanov" }
+            };
+            Mock<IUserFile> fileMock = new Mock<IUserFile>();
+            fileMock.Setup(files => files.ReadAllLines("users.txt")).Returns(users);
+            Mock<IUsersRepositoriy> repoMock = new Mock<IUsersRepositoriy>();
+            repoMock.Setup(repo => repo.GetUser(It.IsAny<string>())).Returns((User)null);
+            IUserFile file = fileMock.Object;
+            IUsersRepositoriy repository = repoMock.Object;
+            ImportFromFile importer = new ImportFromFile(file, repository);
+            bool Flag = importer.ImportUser("users.txt");
+            Assert.IsTrue(Flag);
+            repoMock.Verify(repo => repo.Add(It.IsAny<User>()), Times.Never); // ничего не добавлено
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
--- moq/UnitTestLib/ImportFromFile.cs
+++ moq/UnitTestLib/ImportFromFile.cs
@@ -11,15 +11,17 @@ namespace UnitTestLib
     {
         IUserFile UserFile_;
         IUsersRepositoriy repository_;
-        public ImportFromFile(IUserFile file)
+        public ImportFromFile(IUserFile file, IUsersRepositoriy repository)
         {
             UserFile_ = file;
-
+            repository_ = repository;
         }
         public bool ImportUser (string filePath)
         {
             List<User> users = UserFile_.ReadAllLines(filePath);
+
             List<User> result = new List<User>();
+
             foreach (User user in users)
             {
                 if (user.Login.Contains (" ") ||
@@ -29,9 +31,20 @@ namespace UnitTestLib
                 {
                     continue;
                 }
+
+                if (repository_.GetUser(user.Login) != null)
+                {
+                    continue;
+                }
+
                 result.Add(user);
                 
             }
+            foreach (User user in result)
+            {
+                repository_.Add(user);
+            }
+
             return true;
         }
     }
